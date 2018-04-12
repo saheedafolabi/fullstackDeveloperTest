a71cfcafd12799f84a03b73466d6314de2eeec13
saheedafolabi <38293089+saheedafolabi@users.noreply.github.com>
2018-04-12 05:19:14 +0100

    update

--- connect.php
+++ connect.php
@@ -1,3 +1,3 @@
 <?php
-$con = mysqli_connect("localhost","root","","loginsystem");
+$con = mysqli_connect("https://saheedafolabi.github.io/fullstackDeveloperTest/","saheedafolabi","saholada12","loginsystem");
 ?>
